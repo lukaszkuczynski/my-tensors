0ed59647e872f58510565cc3186c8907f0d3bf87
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-10-31 09:08:11 +0100

    Download checkpoint for chosen model

---
+++ tf1-custom-od/getmodel.sh
@@ -0,0 +1,5 @@
+tfzippath=http://download.tensorflow.org/models/object_detection/$MODEL_NAME.tar.gz
+wget $tfzippath
+tar -xf $MODEL_NAME.tar.gz
+rm $MODEL_NAME.tar.gz
+mv $MODEL_NAME workspace/pre-trained-models/
