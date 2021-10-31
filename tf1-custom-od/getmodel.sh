a75b625be4589ebbe3edd83920bdc7ab7e851a41
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-10-31 10:26:05 +0100

    Alter pipeline.config

--- tf1-custom-od/getmodel.sh
+++ tf1-custom-od/getmodel.sh
@@ -1,7 +1,8 @@
-echo $MODEL_NAME
-exit 1
 tfzippath=http://download.tensorflow.org/models/object_detection/$MODEL_NAME.tar.gz
 wget $tfzippath
 tar -xf $MODEL_NAME.tar.gz
 rm $MODEL_NAME.tar.gz
 mv $MODEL_NAME workspace/pre-trained-models/
+# object detection scripts complain about batch_norm_trainable
+sed -i 's/batch_norm_trainable: true//' workspace/pre-trained-models/$MODEL_NAME/pipeline.config
+      
--- tf1-custom-od/scripts/prepare.py
+++ tf1-custom-od/scripts/prepare.py
@@ -9,14 +9,18 @@ from lxml import etree
 import PIL.Image
 import io
 import hashlib
+from google.protobuf import text_format
+from object_detection.protos import pipeline_pb2
 
 PROJECT_NAME=os.getenv("IMAGE_PROJECT_NAME")
+model_name = os.getenv("MODEL_NAME")
 
 WORKSPACE_DIR = "/root/workspace/"
 
 IMAGES_AND_LABELS_FOLDER = os.path.join(WORKSPACE_DIR, 'images')
 IMAGES_AND_LABELS_PROJECT_FOLDER = os.path.join(IMAGES_AND_LABELS_FOLDER, PROJECT_NAME)
 LABEL_MAP_PATH = os.path.join(WORKSPACE_DIR, "labels.pbtxt")
+PRETRAINED_MODELS_FOLDER = os.path.join(WORKSPACE_DIR, "pre-trained-models")
 
 def train_valid_test_split(path, split_ratio=(0.8,0.1,0.1)):
   all_images = list(os.path.basename(filename) for filename in glob.glob(os.path.join(path, "*.JPEG")))
@@ -142,8 +146,60 @@ def files_to_tfrecord(filenames, data_dir, output_path, ignore_difficult_instanc
       examples.append(tf_example)
       writer.write(tf_example.SerializeToString())
 
+
+
+
+
+original_pipeline_path = os.path.join(PRETRAINED_MODELS_FOLDER, model_name, "pipeline.config")
+pipeline_path = os.path.join(PRETRAINED_MODELS_FOLDER, model_name, "pipeline.confignew")
+checkpoint_path = "checkpoint/ckpt-0"
+
+local_data_folder = "./source_dir"
+
+label_map_filename = "avocado_labels.pbtxt"
+
+
+def edit_pipeline_config(old_path, new_path, cfg):
+  pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()                                                                                                                                                                                                          
+
+  with compat_tf.gfile.GFile(old_path, "r") as f:                                                                                                                                                                                                                     
+      proto_str = f.read()                                                                                                                                                                                                                                          
+      text_format.Merge(proto_str, pipeline_config)                                                                                                                                                                                                                 
+
+  pipeline_config.model.ssd.num_classes = cfg['num_classes']
+  pipeline_config.train_config.batch_size = cfg['batch_size']
+  pipeline_config.train_config.fine_tune_checkpoint = cfg['fine_tune_checkpoint']
+  pipeline_config.train_config.fine_tune_checkpoint_type = 'detection'
+  pipeline_config.train_input_reader.label_map_path = cfg['label_map_path']
+  pipeline_config.train_input_reader.tf_record_input_reader.input_path[0] = cfg['train_tf_path']
+  pipeline_config.eval_input_reader[0].label_map_path = cfg['label_map_path']
+  pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[0] = cfg['eval_tf_path']
+
+  config_text = text_format.MessageToString(pipeline_config)                                                                                                                                                                                                        
+  with compat_tf.gfile.Open(new_path, "wb") as f:                                                                                                                                                                                                                       
+      f.write(config_text)                                                                                                                                                                                                                                          
+
+        
+        
+
+data_folder = local_data_folder
+        
+new_config = {
+    "batch_size": 8,
+    "num_classes": 1,
+    "label_map_path": os.path.join(data_folder, label_map_filename),
+    "train_tf_path": os.path.join(data_folder, 'train'),
+    "eval_tf_path": os.path.join(data_folder, 'valid'),
+    "fine_tune_checkpoint": checkpoint_path
+}
+
+
+
+
+
 files_to_tfrecord(train_images, data_dir, os.path.join(tf_record_folder, 'train'))
 files_to_tfrecord(valid_images, data_dir, os.path.join(tf_record_folder, 'valid'))
 files_to_tfrecord(test_images, data_dir, os.path.join(tf_record_folder, 'test'))
 
 
+edit_pipeline_config(original_pipeline_path, pipeline_path, new_config)
